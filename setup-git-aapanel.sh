b758f3b8192250453996d55ac422dceca9512af0
Ari Ardiansyah <ariardiansyah101@gmail.com>
2021-11-09 16:02:58 +0700

    [UPDATE] Update script aapanel

--- setup-git-aapanel.sh
+++ setup-git-aapanel.sh
@@ -2,14 +2,14 @@ sudo rm -rf /var/repo
 cd /var
 sudo mkdir repo
 cd repo
-sudo mkdir academy.perbarindo.org
-cd academy.perbarindo.org
+sudo mkdir academy.perbarindo.org.git
+cd academy.perbarindo.org.git
 sudo git init --bare
 cd hooks
 sudo touch post-receive
 sudo chmod +x post-receive
 sudo bash -c 'echo "#!/bin/sh" > post-receive'
-sudo bash -c 'echo "git --work-tree=/www/wwwroot/academy.perbarindo.org --git-dir=/var/repo/academy.perbarindo.org checkout -f" > post-receive'
+sudo bash -c 'echo "git --work-tree=/www/wwwroot/academy.perbarindo.org --git-dir=/var/repo/academy.perbarindo.org.git checkout -f" > post-receive'
 
 echo 'DONE'
 cd ..
